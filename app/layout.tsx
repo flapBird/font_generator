b01ab58bc76689177c15d38c2cbd79a717287d55
leeswal <1846873056@qq.com>
2025-12-08 23:39:45 +0800

    add GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,6 +49,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-1LZVVCMCK5"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1LZVVCMCK5');
+          `
+        }} />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
